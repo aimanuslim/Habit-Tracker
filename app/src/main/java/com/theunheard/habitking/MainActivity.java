9fd389ed92fcc9296f9db22891d7e562811c14ed
Aiman Md Uslim <amduslim@purdue.edu>
2017-01-30 22:34:06 +0800

    saving to cloud

--- app/src/main/java/com/theunheard/habitking/MainActivity.java
+++ app/src/main/java/com/theunheard/habitking/MainActivity.java
@@ -1,7 +1,12 @@
 package com.theunheard.habitking;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
@@ -10,8 +15,18 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
+import java.util.Date;
 
 import static android.text.InputType.TYPE_CLASS_DATETIME;
 import static android.text.InputType.TYPE_DATETIME_VARIATION_DATE;
@@ -22,11 +37,19 @@ public class MainActivity extends AppCompatActivity {
     private DatePickerDialog datePickerDialog;
     private TimePickerDialog timePickerDialog;
 
+    private EditText habitNameTextView;
     private EditText dateTextView;
     private EditText timeTextView;
     private EditText reminderDateTextView;
     private EditText reminderTimeTextView;
     private Button addPersonButton;
+    private Button recordButton;
+
+
+
+    // Firebase objects
+    private DatabaseReference _databaseRef;
+    private FirebaseDatabase _firebaseInstance;
 
     private final static String TAG = "HabitKing";
 
@@ -42,6 +65,13 @@ public class MainActivity extends AppCompatActivity {
         reminderDateTextView = (EditText) findViewById(R.id.reminderDateInput);
         reminderTimeTextView = (EditText) findViewById(R.id.reminderTimeInput);
         addPersonButton = (Button) findViewById(R.id.addPersonInteractedButton);
+        recordButton = (Button) findViewById(R.id.recordButton);
+
+
+        // firebase initialization
+        _firebaseInstance = FirebaseDatabase.getInstance();
+        _firebaseInstance.setPersistenceEnabled(true);
+        _databaseRef = _firebaseInstance.getReference();
 
 
 
@@ -64,13 +94,113 @@ public class MainActivity extends AppCompatActivity {
         setupField(reminderTimeTextView);
 
         setupAddInteractedPersonButton(addPersonButton);
+        setupRecordButton(recordButton);
 
 
 
     }
 
-    private void setupAddInteractedPersonButton(Button btn) {
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 
+    private void setupRecordButton(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean valid = areEntriesValid();
+//                if(isNetworkAvailable()) {
+                if(areEntriesValid()) {
+                    saveToCloud();
+                } else {
+                    Toast.makeText(MainActivity.this, "Some entries are required to be filled in!", Toast.LENGTH_SHORT);
+                }
+//                } else {
+                    // TODO: save data to local memory
+                    // TODO: create a background process that checks for connection
+
+//                }
+            }
+        });
+    }
+
+
+    private boolean checkLogInStatus(FirebaseUser user) {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            return true;
+        } else {
+            // No user is signed in
+            Toast.makeText(MainActivity.this, "User not logged in!", Toast.LENGTH_SHORT);
+            return false;
+        }
+    }
+
+    private void saveToCloud() {
+        Date convertedLastDatePerformed = stringToDate(dateTextView.getText().toString());
+        String habitName = habitNameTextView.getText().toString();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(checkLogInStatus(user)) {
+            String uid = user.getUid();
+        } else {
+        }
+
+
+
+
+        Habit habit = new Habit()
+    }
+    private boolean areEntriesValid() {
+        if(habitNameTextView.getText().toString().trim().length() == 0) { return false; }
+        if(dateTextView.getText().toString().trim().length() == 0 && timeTextView.getText().toString().trim().length() == 0) { return false; }
+        return true;
+    }
+
+    private void savetoCloud() {
+        // TODO: save data to firebase
+        Habit habit = new Habit()
+        _databaseRef.child("habits").child
+    }
+
+    private void setupAddInteractedPersonButton(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Add person");
+                alert.setMessage("Enter the name of the person you performed this activity with");
+
+                final EditText input = new EditText(MainActivity.this);
+                alert.setView(input);
+
+                alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String personName = input.getEditableText().toString();
+                        TextView tv = (TextView) new TextView(MainActivity.this);
+                        LinearLayout ll = (LinearLayout) findViewById(R.id.interactedPersonList);
+                        tv.setText(personName);
+                        ll.addView(tv);
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
+
+            }
+        });
     }
 
     private void hideSoftKeyboard(EditText et){
